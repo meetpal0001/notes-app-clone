e1e2ac59f64f33d0bd385abd4db34c761dbd82c7
Meetpal Singh <msingh683@myseneca.ca>
2024-09-16 18:54:24 -0400

    cloned

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPbQM9WW9YtqIj_CbivQXlIqrD5QfaxgI",
-  authDomain: "react-notes-8444e.firebaseapp.com",
-  projectId: "react-notes-8444e",
-  storageBucket: "react-notes-8444e.appspot.com",
-  messagingSenderId: "243902278284",
-  appId: "1:243902278284:web:1b131a51a89a0fd616e9a9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
